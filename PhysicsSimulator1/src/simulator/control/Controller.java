330c8c78ed51b2248d2e2527f7b435e1dd070451
s <sofipost@ucm.es>
2023-02-09 10:54:07 +0100

    cambios laboratorio

--- PhysicsSimulator1/src/simulator/control/Controller.java
+++ PhysicsSimulator1/src/simulator/control/Controller.java
@@ -2,7 +2,9 @@ package simulator.control;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.PrintStream;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
@@ -16,19 +18,50 @@ public class Controller {
 	Factory<Body> factoryBodies;
 	Factory<ForceLaws> factoryLaws;
 
-	Controller(PhysicsSimulator pS, Factory<Body> fb, Factory<ForceLaws> fl) {
+	public Controller(PhysicsSimulator pS, Factory<Body> fb, Factory<ForceLaws> fl) {
 		this.physicsSimulator = pS;
 		this.factoryBodies = fb;
 		this.factoryLaws = fl;
 	}
 
-	public void loadData(InputStream in) {// LO PUEDO HACER YA CREO
+	public void loadData(InputStream in) {//PREGUNTAR A DIEGO
 
-		JSONObject jsonInupt = new JSONObject(new JSONTokener(in));
+		JSONObject jsonInput = new JSONObject(new JSONTokener(in));
+		 JSONArray jaG = jsonInput.getJSONArray("groups");
+		 JSONArray jaL = jsonInput.getJSONArray("laws");
+		 JSONArray jaB = jsonInput.getJSONArray("bodies");
+		 
+		 for(int i = 0; i < jaB.length(); ++i) {
+			 
+			 physicsSimulator.addBody(factoryBodies.createInstance(jaB.getJSONObject(i)));
+		 }
+		 for(int j = 0; j < jaL.length(); ++j) {
+			 
+			 physicsSimulator.setForceLaws(jaL.getString("id"), jaL.);
+		 }
+		 for(int i = 0; i < jaB.length(); ++i) {
+			 
+			 physicsSimulator.addGroup(factory.createInstance(jaG.getJSONObject(i)));
+		 }
 	}
 
-	public void run(int n, OutputStream out) {// LO PUEDO HACER YA?
+	public void run(int n, OutputStream out) {
+		 JSONObject jo = new JSONObject();
+		 JSONArray ja = new JSONArray();
+		 
+		 ja.put(this.physicsSimulator.getState());
+		 
+		 for(int i = 0; i < n; ++i) {
+			 
+			 physicsSimulator.advance();
+			 ja.put(physicsSimulator.getState());
+			 
+		 }
+		 
+		 jo.put("states", ja);
 
+		 PrintStream ps = new PrintStream(out);
+		 ps.println(jo.toString());
+		 ps.close();
 	}
-
 }
--- PhysicsSimulator1/src/simulator/factories/BuilderBasedFactory.java
+++ PhysicsSimulator1/src/simulator/factories/BuilderBasedFactory.java
@@ -1,38 +1,33 @@
 
 package simulator.factories;
 
-import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 import org.json.JSONObject;
-
-import simulator.model.Body;
 
 public class BuilderBasedFactory<T> implements Factory<T> {
 	private Map<String, Builder<T>> _builders;
 	private List<JSONObject> _buildersInfo;
 
 	public BuilderBasedFactory() {
-		// Create a HashMap for _builders, a LinkedList _buildersInfo
-
+		this._builders = new HashMap<String, Builder<T>>();
+		this._buildersInfo =  new LinkedList<JSONObject>();
 	}
 
 	public BuilderBasedFactory(List<Builder<T>> builders) {
 		this();
+		
 		for (Builder<T> b : builders) {
-			// NO SE SI ES ASI
+			addBuilder(b);
 		}
-
-		// call addBuilder(b) for each builder b in builder
-		// ...
 	}
 
 	public void addBuilder(Builder<T> b) {
-		// add and entry ‘‘ b.getTag() −> b’’ to _builders.
 		_builders.put(b.getTypeTag(), b);
-		// add b.getInfo () to _buildersInfo
 		_buildersInfo.add(b.getInfo());
 	}
 
@@ -44,41 +39,21 @@ public class BuilderBasedFactory<T> implements Factory<T> {
 		if (info == null) {
 			throw new IllegalArgumentException("Invalid value for createInstance: null");
 		}
-		// Search for a builder with a tag equals to info . getString ("type"), call its
-		for(Builder<T> b : _builders.values()) {
-			
-			//SI EL TAG DEL CUERPO ES EK MISMO QUE EL TYPE DE INDO
-			if(b.getTypeTag().equals(info.getString("type"))) {
+		
+			if(_builders.containsKey(info.getString("type"))) {
 				
-				instance = b.createInstance(info.getJSONObject("data"));
+				instance = _builders.get(info.getString("type")).createInstance(info.getJSONObject("data"));
 				
 				if(instance == null) {
-					
+					throw new IllegalArgumentException("Invalid value for instance: null");
 				}
 				else {
 					return instance;	
 				}
-						
-		}
-		
-		return null;	
-		// createInstance method and return the result if it is not null . The value you
-		// pass to createInstance is :
-		//
-		// info . has("data") ? info . getJSONObject("data") : new getJSONObject()
-		// If no builder is found or thr result is null ...
-
-		throw new IllegalArgumentException("Invalid value for createInstance: " + info.toString());
-		
-		//NS PK DA ERROR, ASI SE CREA UNA FACTORIA DE CUERPOS:
-		ArrayList<Builder<Body>> bodyBuilders = new ArrayList<>();
-		
-		bodyBuilders.add(new MovingBodyBuilder(null, null));
-		
-		bodyBuilders.add(new StationaryBodyBuilder(null, null));
-		
-		Factory<Body> bodyFactory = new BuilderBasedFactory<Body>(bodyBuilders);
-		}
+			}
+			else
+				throw new IllegalArgumentException("Invalid value for createInstance: " +
+						info.toString());
 	}
 
 	@Override
